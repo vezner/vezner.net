b50f15a0e1bcf47ea0b2f29a04b716b122f9c07d
Fabri <fabri.devv@gmail.com>
2026-07-07 19:27:50 -0300

    feat: add search bar and update contact UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://vezner.net',
+  integrations: [tailwind(), sitemap()],
   vite: {
     optimizeDeps: {
       include: ['gsap', 'gsap/ScrollTrigger', 'lenis', 'three']
